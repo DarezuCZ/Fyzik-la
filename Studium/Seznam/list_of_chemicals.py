e850eaa046143ad6fd019938d55bf0dfbc2d6d7a
Darezu <david.rezazgui@gmail.com>
2023-01-16 20:04:28 +0100

    Seznam 16..2023

--- Studium/Seznam/list_of_chemicals.py
+++ Studium/Seznam/list_of_chemicals.py
@@ -1,18 +1,20 @@
 # GUI #############################################################################################################
 import tkinter as tk
 from tkinter import *
-from tkinter import Entry, Label, Button, simpledialog, ttk, Tk
+from tkinter import Entry, Label, Button, simpledialog, ttk, Tk, Text
 
 
 # import panda knihovny
-import pandas as pd
+import pandas as pds
+from pandas import option_context
 
 # db loading
-db = pd.read_excel('chemical_db.xlsx')
+db = pds.read_excel('chemical_db.xlsx', usecols="A,F,G,H,I", keep_default_na=False)
 
 # Popup window with search and results ############################################################################
 def find():
     chemical = simpledialog.askstring('Find chemical', 'Enter name')
+    synonym = chemical
     while chemical == '':
         chemical = simpledialog.askstring('Find chemical', 'Enter name')
     
@@ -20,33 +22,52 @@ def find():
     results_win.title('Chemicals found:')
     results_win.lift(root)
     results_win.iconbitmap('listICO.ico')
+    results_win.grid_columnconfigure(0, weight=1)
+    results_win.grid_rowconfigure(0, weight=1)
 
-    results = ttk.Label(results_win, text = db[db.compound.str.contains(chemical)])
-    results.pack()
+    with pds.option_context('display.max_rows', None, 'display.max_colwidth', 40):
+        found = tk.Text(results_win, wrap=WORD, height=30, width=105, font=('Arial', 13), spacing1=1, spacing3=1)
+        found.grid(row=0, column=0, sticky=tk.EW)
+        found_scrollbar = ttk.Scrollbar(results_win, orient='vertical', command=found.yview)
+        found_scrollbar.grid(row=0, column=1, sticky=tk.NS)
+        found['yscrollcommand'] = found_scrollbar.set
+        found_compounds = db[db.compound.str.startswith(chemical)]
+        '''found_synonym = db[db.synonyms.str.startswith(chemical)]'''
+        found.insert('0.0', found_compounds)
+        '''results_compound = ttk.Label(results_win, text = db[db.compound.str.contains(chemical)], font=('Arial', 10)).pack(expand=True)
+        results_synonyms = ttk.Label(results_win, text = db[db.synonyms.str.contains(chemical)], font=('Arial', 10)).pack(expand=True)'''
+
 
     try:
         from ctypes import windll
         windll.shcore.SetProcessDpiAwareness(1)
     finally:
         results_win.mainloop()
-        print(db[db.compound.str.contains(chemical)])
+
+
 
 # Root + Buttons ##################################################################################################
 root = Tk(screenName='Chemical database', baseName='Chemical database')
 root.title('Chemical database')
-root.geometry('400x100+600+300')
+root.geometry('350x350+600+300')
 root.iconbitmap('listICO.ico')
+root_background = tk.PhotoImage(file='background_resized.png')
+root_background_set = tk.Label(root, image=root_background, compound='left').pack()
 
-find_chemical = Button(root, text='Find chemical', command=find)
+find_chemical_icon = tk.PhotoImage(file='chemical_res.png')
+find_chemical = tk.Button(root, text='Find chemical', compound=tk.LEFT, font=('Arial', 10), command=find, image=find_chemical_icon, height=40, width=140)
 find_chemical.pack()
 
-add_chemical = Button(root, text='Add chemical')
+add_chemical_icon = tk.PhotoImage(file='plus_res.png')
+add_chemical = tk.Button(root, text='Add chemical', compound=tk.LEFT, font=('Arial', 10), image=add_chemical_icon, height=40, width=140)
 add_chemical.pack()
 
-update_chemical = Button(root, text='Update chemical')
+update_chemical_icon = tk.PhotoImage(file='captcha_res.png')
+update_chemical = tk.Button(root, text='Update chemical', compound=tk.LEFT, font=('Arial', 10), image=update_chemical_icon, height=40, width=140)
 update_chemical.pack()
 
-delete_chemical = Button(root, text='Delete chemical')
+delete_chemical_icon = tk.PhotoImage(file='minus_res.png')
+delete_chemical = tk.Button(root, text='Delete chemical', compound=tk.LEFT, font=('Arial', 10), image=delete_chemical_icon, height=40, width=140)
 delete_chemical.pack()
 
 
